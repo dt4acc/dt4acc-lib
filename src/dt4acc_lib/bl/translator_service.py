b9c5cd3a86178205eb941059189a680a0f9cd183
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-05-27 19:13:01 +0200

    [TASK] translator service: simplify look up of corresponding conversions

--- src/dt4acc_lib/bl/translator_service.py
+++ src/dt4acc_lib/bl/translator_service.py
@@ -68,27 +68,26 @@ class TranslatorService(TranslatorServiceBase):
         logger.error(
             f"{self.__class__.__name__}: I did not find id {id_} in lookup table"
         )
-        od = self.objects_for_device(id_)
+        od = self.objects_for_device(id_.device_property_id.device_name)
         logger.warning(f"{self.__class__.__name__}: For the device I know {od}")
-        em = self.objects_for_lat_elem(id_)
+        em = self.objects_for_lat_elem(id_.lattice_property_id.element_name)
         logger.warning(
             f"{self.__class__.__name__}: For the lattice element I know {em}"
         )
         raise KeyError(f"No translation object for {id_}")
 
-    def objects_for_lat_elem(self, id_: ConversionID):
+    def objects_for_lat_elem(self, element_name: str):
         return {
             key: self.lut.get(key)
             for key in self.lut.keys()
-            if id_.lattice_property_id.element_name
-            == key.lattice_property_id.element_name
+            if element_name == key.lattice_property_id.element_name
         }
 
-    def objects_for_device(self, id_: ConversionID):
+    def objects_for_device(self, device_name: str):
         return {
             key: self.lut.get(key)
             for key in self.lut.keys()
-            if id_.device_property_id.device_name == key.device_property_id.device_name
+            if device_name == key.device_property_id.device_name
         }
 
 
