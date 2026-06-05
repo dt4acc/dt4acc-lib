25a94cfde8dff3dfa3a6fab5771909af1f0367d3
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-06-05 17:01:09 +0200

    [TASK] survey: use name or uid ?

--- src/dt4acc_lib/model/output/survey.py
+++ src/dt4acc_lib/model/output/survey.py
@@ -4,11 +4,9 @@ from dataclasses import dataclass
 @dataclass
 class SurveyDataForElement:
     # Todo: go over all models
-    #       have them return or uid not
-    #       both
-    #       Preferably uid
+    #       have them be tagged by name or uid not both
+    #       Preferably tag it by uid
     #       have a service to do the resolution
-
     name: str
 
     uid: str
