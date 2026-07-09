269ea3afd287d0383e178d5ca9f0478fbfdf4a11
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-07-09 11:56:48 +0200

    [FIX] skip test if dt4acc is not available
    Todo: Need to add a test lattice to dt4acc-lib

--- tests/test_pyat_simulator.py
+++ tests/test_pyat_simulator.py
@@ -1,16 +1,23 @@
 from importlib import resources
+import pytest
 
-from dt4acc.custom_facility.bessyii.run_bessyii_twin import bessyii_pyat_lattice
 from dt4acc_lib.pyat_simulator.accelerator_simulator import PyATAcceleratorSimulator
 from dt4acc_lib.pyat_simulator.simulator_backend import SimulatorBackend
 
 
-def test_get_survey() -> None:
+@pytest.fixture(scope="module")
+def bessyii_lattice():
+    bessyii_pyat_lattice = pytest.importorskip(
+        "dt4acc.custom_facility.bessyii.run_bessyii_twin.bessyii_pyat_lattice"
+    )
     filename = resources.files("dt4acc").joinpath(
         "custom_facility/bessyii/resources/storage_ring/input/bessy2_storage_ring_reflat.json"
     )
+    return bessyii_pyat_lattice(filename=filename)
 
-    acc = bessyii_pyat_lattice(filename=filename)
-    acc = PyATAcceleratorSimulator(at_lattice=acc)
+
+def test_get_survey(bessyii_lattice) -> None:
+
+    acc = PyATAcceleratorSimulator(at_lattice=bessyii_lattice)
     data = acc.get_survey()
     data
